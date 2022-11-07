304bab70aeb011a3afa944ca6c6284c9ace29741
Tiago Santiago <87858900+Ti4goS@users.noreply.github.com>
2022-11-07 16:28:27 -0300

    resolve #3 adicionando classe Jogador.cs

---
+++ Trilha/Trilha/Model/CorJogador.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trilha.Model;
+
+/// <summary>
+/// Define se o jogador é o azul ou o vermelho
+/// </summary>
+public enum CorJogador
+{
+    Azul,
+    Vermelho
+}
+
