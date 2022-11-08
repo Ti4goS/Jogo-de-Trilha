0f028ef768ce31a6a01df2ed796cbc6906107b9d
CaioMorais02 <caioedpm@gmail.com>
2022-11-08 09:02:16 -0300

    resolve #10 adicionei um tabuleiro para a partida

--- Trilha/Trilha/View/Table_Top.xaml.cs
+++ Trilha/Trilha/View/Table_Top.xaml.cs
@@ -24,10 +24,5 @@ namespace Trilha.View
         {
             InitializeComponent();
         }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
